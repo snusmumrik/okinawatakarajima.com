ee22ac0ac6e25dafdf2d59900c3ac52a57de16e1
Hiroyuki Kondo <hiroyuki@chishaku.com>
2015-04-02 17:37:04 +0900

    bug fix for mailer

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -2,6 +2,6 @@
 class PostMailer < ApplicationMailer
   def new_post_email(post)
     @post = post
-    mail(to: @post.email, subject: "【沖縄宝島】ご投稿ありがとうございました") if @post.email
+    mail(to: @post.email, subject: "【沖縄宝島】ご投稿ありがとうございました") unless @post.email.blank?
   end
 end
--- app/mailers/reply_mailer.rb
+++ app/mailers/reply_mailer.rb
@@ -2,12 +2,12 @@
 class ReplyMailer < ApplicationMailer
   def new_reply_email(reply)
     @reply = reply
-    mail(to: @reply.email, subject: "【沖縄宝島】ご利用ありがとうございました")
+    mail(to: @reply.email, subject: "【沖縄宝島】ご利用ありがとうございました") unless @reply.email.blank?
   end
 
   def reply_notification_email(reply)
     @reply = reply
     @post = @reply.post
-    mail(to: @post.email, subject: "【沖縄宝島】投稿に対して返信がありました")
+    mail(to: @post.email, subject: "【沖縄宝島】投稿に対して返信がありました") unless @post.email.blank?
   end
 end
