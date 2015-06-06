e40345a4c7b2b3d142b7c25c96f7cf73bbf18043
Hiroyuki Kondo <hiroyuki@chishaku.com>
2015-06-06 14:04:13 +0900

    add simple_captcha

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -2,6 +2,7 @@
 
 class RepliesController < ApplicationController
   before_action :set_reply, only: [:show, :edit, :update, :destroy]
+  before_action :validate_simple_captcha, only: [:create]
 
   respond_to :html
 
@@ -34,6 +35,7 @@ class RepliesController < ApplicationController
       ReplyMailer.reply_notification_email(@reply).deliver_now
 
       session[:post_id] = nil
+      flash[:notice] = "返信メールを送信しました。。"
       respond_with(@post, location: board_path(@post.board), notice: "返信しました。")
     else
       respond_with(@reply)
@@ -56,6 +58,13 @@ class RepliesController < ApplicationController
   end
 
   def reply_params
-    params.require(:reply).permit(:user_id, :post_id, :name, :email, :text)
+    params.require(:reply).permit(:user_id, :post_id, :name, :email, :text, :captcha, :captcha_key)
+  end
+
+  def validate_simple_captcha
+    unless simple_captcha_valid?
+      flash[:alert] = t("simple_captcha.message.default")
+      redirect_to "#{new_reply_path}/#{session[:post_id]}"
+    end
   end
 end
